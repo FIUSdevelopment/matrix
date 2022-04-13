43c00a5edb07368b0db30e59cd30ab7857ed6a5a
Killer Boss Original <73131550+killerbossoriginal@users.noreply.github.com>
2022-04-13 20:14:21 +0200

    0.0.1
    Created 'Setup'

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    db: 'mapdb', // disabled, mapdb
+    discord: {
+        enabled: true,
+        discordjsversion: 'latest' // latest or search on github
+    },
+    telegram: {
+        enabled: false
+    },
+    webserver: {
+        enabled: false
+    }
+}
